ebf15cce9679c8426e726c4a1808102e6de9a380
Graham Keeling <graham@localhost.localdomain>
2011-09-28 21:53:35 +0100

    Fix possible segfault when returning error from forward_sbuf.

--- src/backup_phase1_server.c
+++ src/backup_phase1_server.c
@@ -114,7 +114,6 @@ static int forward_sbuf(FILE *fp, gzFile zp, struct sbuf *b, struct sbuf *target
 {
 	int ars=0;
 	struct sbuf latest;
-	init_sbuf(b);
 	init_sbuf(&latest);
 	off_t pos=0;
 	while(1)
@@ -205,6 +204,9 @@ int do_resume(gzFile p1zp, FILE *p2fp, FILE *ucfp, gzFile cmanfp, struct dpth *d
 	struct sbuf p1b;
 	struct sbuf p2b;
 	struct sbuf ucb;
+	init_sbuf(&p1b);
+	init_sbuf(&p2b);
+	init_sbuf(&ucb);
 
 	logp("Begin phase1 (read previous file system scan)\n");
 	if(read_phase1(p1zp, p1cntr)) goto error;

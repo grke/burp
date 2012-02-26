d198434a4313959d4ce2bc40cd0223f7f9ea6700
Graham Keeling <keeling@spamcop.net>
2012-02-26 14:24:42 +0000

    Move binaries to /usr/sbin.

--- debian/burp.cron.d
+++ debian/burp.cron.d
@@ -5,4 +5,4 @@
 # will decide whether it is yet time to do a backup or not.
 # It might be a good idea to change the numbers below for different clients,
 # in order to spread the load a bit.
-#7,27,47 * * * *	root	[ -x /sbin/burp ] && /sbin/burp -a t >>/var/log/burp-client 2>&1
+#7,27,47 * * * *	root	[ -x /usr/sbin/burp ] && /usr/sbin/burp -a t >>/var/log/burp-client 2>&1

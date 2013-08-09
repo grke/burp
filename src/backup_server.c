f6c87fa83af2f8d7d22dc8e4b1d7a9a4c32adcae
graham <graham@space.grke.net>
2013-08-09 12:10:49 +1000

    Cleanup of the free-ing.

--- src/backup_server.c
+++ src/backup_server.c
@@ -464,6 +464,7 @@ static void get_wbuf_from_files(struct iobuf *wbuf, struct slist *slist, int sca
 
 	// Only need to request the path at this stage.
 	iobuf_from_sbuf_path(wbuf, sb);
+printf("want sigs for: %s\n", sb->path);
 	sb->sent_path=1;
 	sb->index=file_no++;
 }

a4c60abee4de8fe8ba8f520715a42ebc1b4b2f8f
graham <graham@leica-geosystems.com>
2013-08-11 06:05:41 +1000

    Finally got zero length files and async io working sensibly.

--- src/backup_client.c
+++ src/backup_client.c
@@ -71,8 +71,7 @@ static int add_to_data_requests(struct blist *blist, struct iobuf *rbuf)
 		if(index==blk->index) break;
 	if(!blk)
 	{
-		logp("Could not find requested block %lu\n",
-			index);
+		logp("Could not find requested block %lu\n", index);
 		return -1;
 	}
 	blk->requested=1;
@@ -261,7 +260,7 @@ static void get_wbuf_from_blks(struct iobuf *wbuf, struct slist *slist, int requ
 
 	if(!sb->sent_stat)
 	{
-//printf("want to send stat: %s\n", sb->path);
+printf("want to send stat: %s\n", sb->path);
 		iobuf_from_sbuf_attr(wbuf, sb);
 		wbuf->cmd=CMD_ATTRIBS_SIGS; // hack
 		sb->sent_stat=1;

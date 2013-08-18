6b4f2e47d9c315de9feca845456f7f1b9fc9d1c5
graham <graham@space.grke.net>
2013-08-18 20:21:52 +1000

    Write machine readable backup stats file to each storage directory.

--- src/counter.h
+++ src/counter.h
@@ -90,6 +90,7 @@ struct cntr
 };
 
 extern void print_filecounters(struct cntr *p1c, struct cntr *c, enum action act);
+extern int print_stats_to_file(struct cntr *p1c, struct cntr *c, struct config *conf, const char *client, const char *directory, enum action act);
 extern void print_endcounter(struct cntr *c);
 extern void do_filecounter(struct cntr *c, char ch, int print);
 extern void do_filecounter_same(struct cntr *c, char ch);

a0ea55b8ffe9dfa1cf2976af61a57ccdb92eb6db
graham <graham@leica-geosystems.com>
2013-11-13 06:41:18 +0700

    Clean JSON output.

--- src/server/main.h
+++ src/server/main.h
@@ -5,7 +5,7 @@ extern int server(struct config *conf, const char *configfile,
 	int generate_ca_only);
 
 extern int reload(struct config *conf, const char *configfile, bool firsttime,
-	int oldmax_children, int oldmax_status_children);
+	int oldmax_children, int oldmax_status_children, int json);
 
 extern int setup_signals(int oldmax_children, int max_children,
 	int oldmax_status_children, int max_status_children);

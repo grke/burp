04af96fe15e27c5be2419f8a395ebcf74e32443d
graham <graham@leica-geosystems.com>
2014-04-15 16:11:55 +1000

    Some rearranging.

--- src/burp1/server/backup_phase2.h
+++ src/burp1/server/backup_phase2.h
@@ -1,5 +1,5 @@
-#ifndef _BACKUP_PHASE2_SERVER_H
-#define _BACKUP_PHASE2_SERVER_H
+#ifndef _BURP1_BACKUP_PHASE2_SERVER_H
+#define _BURP1_BACKUP_PHASE2_SERVER_H
 
 extern int backup_phase2_server(struct sdirs *sdirs, struct conf *cconf,
 	gzFile *cmanfp, struct dpthl *dpthl, int resume);
--- src/burp1/server/backup_phase3.h
+++ src/burp1/server/backup_phase3.h
@@ -1,5 +1,5 @@
-#ifndef _BACKUP_PHASE3_SERVER_H
-#define _BACKUP_PHASE3_SERVER_H
+#ifndef _BURP1_BACKUP_PHASE3_SERVER_H
+#define _BURP1_BACKUP_PHASE3_SERVER_H
 
 extern int backup_phase3_server(struct sdirs *sdirs, struct conf *cconf,
 	int recovery, int compress);

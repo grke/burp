a1e44d844eb1c23d32583de5025c62cd41016938
graham <graham@space.grke.net>
2014-04-21 12:07:51 +0000

    Small progress on expanding the champ scoring stuff. Compilation broken.

--- src/burp2/server/champ_chooser/champ_server.c
+++ src/burp2/server/champ_chooser/champ_server.c
@@ -12,6 +12,37 @@ struct clifd
 	struct clifd *next;
 };
 
+static int scores_setup_for_new_client(struct clifd *clifds,
+	struct clifd *newfd)
+{
+	int s;
+	int max=0;
+	struct clifd *c;
+
+	// Find an unused score_index, or the next score_index.
+	// Also find the maximum score index.
+	for(s=0; ; s++)
+	{
+		for(c=clifds; c; c=c->next)
+		{
+			if(c->score_index>max) max=c->score_index;
+			if(s==c->score_index) break;
+		}
+		if(!c) break; // Got to the end of clifds.
+	}
+	newfd->score_index=s;
+	if(s>max) max=s;
+	max++;
+	
+	if(!scores && !(scores=scores_alloc())) goto error;
+	if(scores_grow(scores, candidates_len)) goto error;
+	candidates_set_score_pointers(candidates, candidates_len, scores);
+	scores_reset(scores);
+//	dump_scores("init", scores, scores->size);
+error:
+	return -1;
+}
+
 static int champ_chooser_incoming_client(int s, struct clifd **clifds)
 {
 	socklen_t t;
@@ -31,6 +62,7 @@ static int champ_chooser_incoming_client(int s, struct clifd **clifds)
 			__func__, strerror(errno));
 		goto error;
 	}
+	if(scores_setup_for_new_client(*clifds, newfd)) goto error;
 	newfd->next=*clifds;
 	*clifds=newfd;
 
@@ -134,7 +166,7 @@ int champ_chooser_server(struct sdirs *sdirs, struct conf *conf)
 	}
 
 	// Load the sparse indexes for this dedup group.
-	if(champ_chooser_init(sdirs->datadir, conf))
+	if(champ_chooser_init(sdirs->data, conf))
 		goto end;
 
 	while(!champ_loop(s, &clifds, conf)) { }

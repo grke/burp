36d687316ac402924a9feab1680f057395e5c374
graham <graham@space.grke.net>
2014-05-05 11:38:28 +0000

    Add the ability to run a separate champ chooser process, for debugging.

--- src/burp2/server/champ_chooser/champ_server.h
+++ src/burp2/server/champ_chooser/champ_server.h
@@ -2,5 +2,7 @@
 #define _CHAMP_SERVER_H
 
 extern int champ_chooser_server(struct sdirs *sdirs, struct conf *conf);
+extern int champ_chooser_server_standalone(struct conf *conf,
+	const char *sclient);
 
 #endif

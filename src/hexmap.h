508b5a6715d2d057726953a42005ba78a213ca4b
graham <graham@space.grke.net>
2014-06-17 15:59:36 +1000

    Small tweaks.

--- src/hexmap.h
+++ src/hexmap.h
@@ -4,7 +4,11 @@
 extern uint8_t md5sum_of_empty_string[];
 
 extern void hexmap_init(void);
+
 extern void md5str_to_bytes(const char *md5str, uint8_t *bytes);
 extern char *bytes_to_md5str(uint8_t *bytes);
+
+extern void savepathstr_to_bytes(const char *savepathstr, uint8_t *bytes);
+extern char *bytes_to_savepathstr(uint8_t *bytes);
 
 #endif

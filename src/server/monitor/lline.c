96efe1bafa8b51c6b0f6038ca126b0bb4bb04d92
graham <graham@space.grke.net>
2014-08-24 11:56:53 +0000

    Load log lines from JSON.

---
+++ src/server/monitor/lline.c
@@ -0,0 +1,64 @@
+#include "include.h"
+
+static void lline_free(struct lline *lline)
+{
+	if(!lline) return;
+	if(lline->line) free(lline->line);
+	free(lline);
+}
+
+void llines_free(struct lline **lline)
+{
+	struct lline *l;
+	struct lline *lhead=*lline;
+	while(lhead)
+	{
+		l=lhead;
+		lhead=lhead->next;
+		lline_free(l);
+	}
+	*lline=NULL;
+}
+
+static struct lline *lline_alloc(char *line)
+{
+	struct lline *llnew=NULL;
+	if(!line)
+	{
+		logp("%s called with NULL line!\n", __func__);
+		return NULL;
+	}
+	if(!(llnew=(struct lline *)
+		calloc_w(1, sizeof(struct lline), __func__))
+	  || !(llnew->line=strdup_w(line, __func__)))
+		return NULL;
+	return llnew;
+}
+
+int lline_add(struct lline **lline, char *line)
+{
+	struct lline *l=NULL;
+	struct lline *llast=NULL;
+	struct lline *llnew=NULL;
+
+	if(!(llnew=lline_alloc(line))) return -1;
+
+	for(l=*lline; l; l=l->next)
+	{
+		l->prev=llast;
+		llast=l;
+	}
+	if(llast)
+	{
+		llnew->next=llast->next;
+		llast->next=llnew;
+		llnew->prev=llast;
+	}
+	else
+	{
+		*lline=llnew;
+		llnew->next=l;
+	}
+
+	return 0;
+}

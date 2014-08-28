a75a7ffb7c709999d4912f4181c0574b380bb2b8
graham <graham@space.grke.net>
2014-08-28 13:48:09 +0000

    Move call to sdirs init/alloc to server/run_action.c - preparation for status monitor stuff via the normal client.

--- src/server/run_action.h
+++ src/server/run_action.h
@@ -2,7 +2,6 @@
 #define _RUN_ACTION_SERVER_H
 
 extern int run_action_server(struct async *as,
-	struct sdirs *sdirs, const char *incexc,
-	int srestore, int *timer_ret, struct conf *cconf);
+	const char *incexc, int srestore, int *timer_ret, struct conf *cconf);
 
 #endif

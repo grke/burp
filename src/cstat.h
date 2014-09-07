5f89cffb1c8426ca4424b05642b7283945b5fbec
graham <graham@space.grke.net>
2014-09-07 12:07:27 +0000

    Experiment, trying to split cstat.

---
+++ src/cstat.h
@@ -0,0 +1,70 @@
+#ifndef _CSTAT_H
+#define _CSTAT_H
+
+#include "include.h"
+
+#define STATUS_STR_IDLE			"idle"
+#define STATUS_STR_CLIENT_CRASHED	"c crashed"
+#define STATUS_STR_SERVER_CRASHED	"s crashed"
+#define STATUS_STR_RUNNING		"running"
+#define STATUS_STR_SCANNING		"scanning"
+#define STATUS_STR_BACKUP		"backup"
+#define STATUS_STR_MERGING		"merging"
+#define STATUS_STR_SHUFFLING		"shuffling"
+#define STATUS_STR_LISTING		"listing"
+#define STATUS_STR_RESTORING		"restoring"
+#define STATUS_STR_VERIFYING		"verifying"
+#define STATUS_STR_DELETING		"deleting"
+
+enum cstat_status
+{
+	STATUS_UNSET=0,
+
+	STATUS_IDLE,
+	STATUS_RUNNING,
+	STATUS_CLIENT_CRASHED,
+	STATUS_SERVER_CRASHED,
+
+	STATUS_SCANNING,
+	STATUS_BACKUP,
+	STATUS_MERGING,
+	STATUS_SHUFFLING,
+	STATUS_LISTING,
+	STATUS_RESTORING,
+	STATUS_VERIFYING,
+	STATUS_DELETING
+};
+
+struct cstat
+{
+	char *name;
+	char *conffile;
+	time_t conf_mtime;
+	char *running_detail; // set from the parent process
+	enum cstat_status status;
+
+	// When the mtime of conffile changes, the following get reloaded
+	// sdirs declared as void so that cstat can be declared for both
+	// client and server. Server side will have to cast it.
+	void *sdirs;
+	time_t clientdir_mtime;
+	time_t lockfile_mtime;
+
+	struct bu *bu; // Backup list.
+
+	struct cstat *prev;
+	struct cstat *next;
+};
+
+extern struct cstat *cstat_alloc(void);
+extern int cstat_init(struct cstat *cstat,
+        const char *name, const char *clientconfdir);
+extern void cstat_free(struct cstat **cstat);
+extern int cstat_add_to_list(struct cstat **clist, struct cstat *cnew);
+
+extern const char *cstat_status_to_str(struct cstat *cstat);
+extern cstat_status cstat_str_to_status(const char *str);
+
+extern struct cstat *cstat_get_by_name(struct cstat *clist, const char *name);
+
+#endif
--- src/server/monitor/cstat.c
+++ src/server/monitor/cstat.c
@@ -2,70 +2,10 @@
 
 #include <dirent.h>
 
-struct cstat *cstat_alloc(void)
-{
-	return (struct cstat *)calloc_w(1, sizeof(struct cstat), __func__);
-}
-
-int cstat_init(struct cstat *cstat,
-	const char *name, const char *clientconfdir)
-{
-	if((clientconfdir && !(cstat->conffile=prepend_s(clientconfdir, name)))
-	  || !(cstat->name=strdup_w(name, __func__))
-	  || !(cstat->sdirs=sdirs_alloc()))
-		return -1;
-	return 0;
-}
-
-static void cstat_free_content(struct cstat *c)
-{
-	if(!c) return;
-	bu_list_free(&c->bu);
-	free_w(&c->name);
-	free_w(&c->conffile);
-	free_w(&c->running_detail);
-	sdirs_free_content(c->sdirs);
-	c->clientdir_mtime=0;
-	c->lockfile_mtime=0;
-}
-
-void cstat_free(struct cstat **cstat)
-{
-	if(!cstat || !*cstat) return;
-	cstat_free_content(*cstat);
-	free_v((void **)cstat);
-}
-
-int cstat_add_to_list(struct cstat **clist, struct cstat *cnew)
-{
-	struct cstat *c=NULL;
-	struct cstat *clast=NULL;
-
-	for(c=*clist; c; c=c->next)
-	{
-		if(strcmp(cnew->name, c->name)<0) break;
-		c->prev=clast;
-		clast=c;
-	}
-	if(clast)
-	{
-		cnew->next=clast->next;
-		clast->next=cnew;
-		cnew->prev=clast;
-	}
-	else
-	{
-		*clist=cnew;
-		cnew->next=c;
-	}
-
-	return 0;
-}
-
 static int set_cstat_from_conf(struct cstat *c, struct conf *cconf)
 {
-	sdirs_free_content(c->sdirs);
-	if(sdirs_init(c->sdirs, cconf)) return -1;
+	sdirs_free_content((struct sdirs *)c->sdirs);
+	if(sdirs_init((struct sdirs *)c->sdirs, cconf)) return -1;
 	return 0;
 }
 
@@ -193,53 +133,14 @@ error:
 	return -1;
 }
 
-const char *cstat_status_to_str(struct cstat *cstat)
-{
-	switch(cstat->status)
-	{
-		case STATUS_IDLE: return STATUS_STR_IDLE;
-		case STATUS_CLIENT_CRASHED: return STATUS_STR_CLIENT_CRASHED;
-		case STATUS_SERVER_CRASHED: return STATUS_STR_SERVER_CRASHED;
-		case STATUS_RUNNING: return STATUS_STR_RUNNING;
-		case STATUS_SCANNING: return STATUS_STR_SCANNING;
-		case STATUS_BACKUP: return STATUS_STR_BACKUP;
-		case STATUS_MERGING: return STATUS_STR_MERGING;
-		case STATUS_SHUFFLING: return STATUS_STR_SHUFFLING;
-		case STATUS_LISTING: return STATUS_STR_LISTING;
-		case STATUS_RESTORING: return STATUS_STR_RESTORING;
-		case STATUS_VERIFYING: return STATUS_STR_VERIFYING;
-		case STATUS_DELETING: return STATUS_STR_DELETING;
-		default: return "unknown";
-	}
-}
-
-enum cstat_status cstat_str_to_status(const char *str)
-{
-	if(!strcmp(str, STATUS_STR_IDLE)) return STATUS_IDLE;
-	else if(!strcmp(str, STATUS_STR_RUNNING)) return STATUS_RUNNING;
-	else if(!strcmp(str, STATUS_STR_CLIENT_CRASHED))
-		return STATUS_CLIENT_CRASHED;
-	else if(!strcmp(str, STATUS_STR_SERVER_CRASHED))
-		return STATUS_CLIENT_CRASHED;
-	else if(!strcmp(str, STATUS_STR_RUNNING)) return STATUS_RUNNING;
-	else if(!strcmp(str, STATUS_STR_SCANNING)) return STATUS_SCANNING;
-	else if(!strcmp(str, STATUS_STR_BACKUP)) return STATUS_BACKUP;
-	else if(!strcmp(str, STATUS_STR_MERGING)) return STATUS_MERGING;
-	else if(!strcmp(str, STATUS_STR_SHUFFLING)) return STATUS_SHUFFLING;
-	else if(!strcmp(str, STATUS_STR_LISTING)) return STATUS_LISTING;
-	else if(!strcmp(str, STATUS_STR_RESTORING)) return STATUS_RESTORING;
-	else if(!strcmp(str, STATUS_STR_VERIFYING)) return STATUS_VERIFYING;
-	else if(!strcmp(str, STATUS_STR_DELETING)) return STATUS_DELETING;
-	return STATUS_UNSET;
-}
-
 int cstat_set_status(struct cstat *cstat)
 {
 	struct stat statp;
+	struct sdirs *sdirs=(struct sdirs *)cstat->sdirs;
 
-	if(lstat(cstat->sdirs->lock->path, &statp))
+	if(lstat(sdirs->lock->path, &statp))
 	{
-		if(lstat(cstat->sdirs->working, &statp))
+		if(lstat(sdirs->working, &statp))
 			cstat->status=STATUS_IDLE;
 		else
 			cstat->status=STATUS_CLIENT_CRASHED;
@@ -248,7 +149,7 @@ int cstat_set_status(struct cstat *cstat)
 	}
 	else
 	{
-		if(!lock_test(cstat->sdirs->lock->path)) // Could have got lock.
+		if(!lock_test(sdirs->lock->path)) // Could have got lock.
 		{
 			cstat->status=STATUS_SERVER_CRASHED;
 			// It is not running, so free the running_detail.
@@ -269,8 +170,9 @@ static int reload_from_clientdir(struct cstat **clist, struct conf *conf)
 		time_t ltime=0;
 		struct stat statp;
 		struct stat lstatp;
-		if(!c->sdirs->client) continue;
-		if(stat(c->sdirs->client, &statp))
+		struct sdirs *sdirs=(struct sdirs *)c->sdirs;
+		if(!sdirs->client) continue;
+		if(stat(sdirs->client, &statp))
 		{
 			// No clientdir.
 			if(!c->status
@@ -278,7 +180,7 @@ static int reload_from_clientdir(struct cstat **clist, struct conf *conf)
 				goto error;
 			continue;
 		}
-		if(!lstat(c->sdirs->lock->path, &lstatp))
+		if(!lstat(sdirs->lock->path, &lstatp))
 			ltime=lstatp.st_mtime;
 		if(statp.st_mtime==c->clientdir_mtime
 		  && ltime==c->lockfile_mtime
@@ -295,7 +197,7 @@ static int reload_from_clientdir(struct cstat **clist, struct conf *conf)
 
 printf("reload from clientdir\n");
 		bu_list_free(&c->bu);
-		if(bu_get_current(c->sdirs, &c->bu))
+		if(bu_get_current(sdirs, &c->bu))
 			goto error;
 	}
 	return 0;
@@ -317,7 +219,7 @@ int cstat_set_backup_list(struct cstat *cstat)
 	// Free any previous list.
 	bu_list_free(&cstat->bu);
 
-	if(bu_get_list_with_working(cstat->sdirs, &bu))
+	if(bu_get_list_with_working((struct sdirs *)cstat->sdirs, &bu))
 	{
 		//logp("error when looking up current backups\n");
 		return 0;
@@ -329,11 +231,4 @@ int cstat_set_backup_list(struct cstat *cstat)
 
 	cstat->bu=bu;
 	return 0;
-}
-
-struct cstat *cstat_get_by_name(struct cstat *clist, const char *name)
-{
-	struct cstat *c;
-        for(c=clist; c; c=c->next) if(!strcmp(c->name, name)) return c;
-        return NULL;
 }

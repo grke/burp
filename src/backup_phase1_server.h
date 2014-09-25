651bcafa44453fe25f772cde802276b06a0f612e
graham <graham@space.grke.net>
2014-09-25 12:28:23 +0000

    Bug fix for resume bug not correctly truncating the phase2/unchanged manifests.

--- src/backup_phase1_server.h
+++ src/backup_phase1_server.h
@@ -1,7 +1,11 @@
 #ifndef BACKUP_PHASE1_SERVER_H
 #define BACKUP_PHASE1_SERVER_H
 
-extern int backup_phase1_server(const char *phase1data, const char *client, struct cntr *p1cntr, struct cntr *cntr, struct config *cconf);
-extern int do_resume(gzFile p1zp, FILE *p2fp, FILE *ucfp, struct dpth *dpth, struct config *cconf, const char *client, struct cntr *p1cntr, struct cntr *cntr);
+extern int backup_phase1_server(const char *phase1data, const char *client,
+	struct cntr *p1cntr, struct cntr *cntr, struct config *cconf);
+extern int do_resume(gzFile p1zp,
+	const char *phase2data, const char *unchangeddata,
+	struct dpth *dpth, struct config *cconf,
+	const char *client, struct cntr *p1cntr, struct cntr *cntr);
 
 #endif

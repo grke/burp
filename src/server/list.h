c4ccc5502df8af3835db6a1789ee235ab2eb7f6a
graham <graham@space.grke.net>
2014-10-03 13:30:07 +0000

    Fix for made up directories not having the directory bit set when browsing.

--- src/server/list.h
+++ src/server/list.h
@@ -2,7 +2,7 @@
 #define _LIST_SERVER_H
 
 extern int check_browsedir(const char *browsedir,
-	char **path, size_t bdlen, char **last_bd_match);
+	struct sbuf *mb, size_t bdlen, char **last_bd_match);
 extern int do_list_server(struct asfd *asfd,
 	struct sdirs *sdirs, struct conf *conf,
 	const char *backup, const char *listregex, const char *browsedir);

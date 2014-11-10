c08058338d33244021671c2298209feeab60b8b4
graham <graham@space.grke.net>
2014-11-10 11:28:02 +0000

    Tidy up the new soft/hard quota feature.

--- src/server/quota.c
+++ src/server/quota.c
@@ -1,31 +1,30 @@
 #include "include.h"
 
+static void quota_log_bytes(struct async *as,
+	const char *msg, unsigned long long byte, unsigned long quota)
+{
+	as->asfd->write_str(as->asfd, CMD_WARNING, msg);
+	logp("Bytes estimated: %Lu%s\n", byte, bytes_to_human(byte));
+	logp("%s: %Lu%s\n", msg, quota, bytes_to_human(quota));
+}
+
 // Return O for OK, -1 if the estimated size is greater than hard_quota 
 int check_quota(struct async *as, struct conf *cconf)
 {
-	int ret=0;
 	unsigned long long byte;
 
 	byte=cconf->cntr->ent[(uint8_t)CMD_BYTES_ESTIMATED]->count;
 
-	// Print error if the estimated size is greater than hard_quota
-	if(cconf->hard_quota != 0 && byte > (unsigned long long)cconf->hard_quota)
+	if(cconf->hard_quota && byte > (unsigned long long)cconf->hard_quota)
 	{
-		logw(as->asfd, cconf, "Err: hard quota is reached");
-		logp("bytes estimated: %Lu%s\n", byte, bytes_to_human(byte));
-		logp("hard quota: %Lu%s\n", cconf->hard_quota, bytes_to_human(cconf->hard_quota));
-		ret=-1;
+		quota_log_bytes(as,
+			"Hard quota exceeded", byte, cconf->soft_quota);
+		return -1;
 	}
-	else
-	{
-		// Print warning if the estimated size is greater than soft_quota
-		if(cconf->soft_quota != 0 && byte > (unsigned long long)cconf->soft_quota)
-		{
-			logw(as->asfd, cconf, "soft quota is exceeded");
-			logp("bytes estimated: %Lu%s\n", byte, bytes_to_human(byte));
-			logp("soft quota: %Lu%s\n", cconf->soft_quota, bytes_to_human(cconf->soft_quota));
-		}  
-	}
+
+	if(cconf->soft_quota && byte > (unsigned long long)cconf->soft_quota)
+		quota_log_bytes(as,
+			"Soft quota exceeded", byte, cconf->soft_quota);
 	
-	return ret;
+	return 0;
 }

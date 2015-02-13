ccecd5c76599887121f75fad7e2f0982f58bdbb8
graham <graham@space.grke.net>
2015-02-13 11:58:26 +0000

    Some more server restore cleanup.

--- src/server/burp2/restore.h
+++ src/server/burp2/restore.h
@@ -1,11 +1,9 @@
 #ifndef _RESTORE_SERVER_BURP2_H
 #define _RESTORE_SERVER_BURP2_H
 
-extern int restore_stream_burp2(struct asfd *asfd,
-	struct sdirs *sdirs, struct slist *slist,
-	struct bu *bu, const char *manifest, regex_t *regex,
-	int srestore, struct conf *conf, enum action act,
-	enum cntr_status cntr_status);
+extern int burp2_extra_restore_stream_bits(struct asfd *asfd, struct blk *blk,
+	struct slist *slist, int need_data, int last_ent_was_dir,
+	struct conf *cconf);
 
 extern int restore_sbuf_burp2(struct asfd *asfd, struct sbuf *sb,
 	enum action act, enum cntr_status cntr_status,

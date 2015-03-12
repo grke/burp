2b8ef6c61ccb03fc908d06a3e87e7a527ce05938
graham <graham@space.grke.net>
2015-03-12 22:32:06 +0000

    More unit tests.

--- utest/test_conffile.c
+++ utest/test_conffile.c
@@ -1,9 +1,25 @@
 #include <check.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include "test.h"
 #include "../src/alloc.h"
 #include "../src/conf.h"
 #include "../src/conffile.h"
+
+static struct conf **setup(void)
+{
+	struct conf **confs;
+	alloc_counters_reset();
+	confs=confs_alloc();
+	confs_init(confs);
+	return confs;
+}
+
+static void tear_down(struct conf ***confs)
+{
+	confs_free(confs);
+	ck_assert_int_eq(free_count, alloc_count);
+}
 
 struct data
 {
@@ -31,8 +47,7 @@ static struct data d[] = {
 
 START_TEST(test_conf_get_pair)
 {
-        unsigned int i;
-        for(i=0; i<sizeof(d)/sizeof(*d); i++)
+        FOREACH(d)
 	{
 		char *field=NULL;
 		char *value=NULL;
@@ -65,10 +80,7 @@ END_TEST
 
 START_TEST(test_client_conf)
 {
-	struct conf **confs;
-	alloc_counters_reset();
-	confs=confs_alloc();
-	confs_init(confs);
+	struct conf **confs=setup();
 	ck_assert_int_eq(conf_load_global_only_buf(MIN_CLIENT_CONF, confs), 0);
 	ck_assert_str_eq(get_string(confs[OPT_SERVER]), "4.5.6.7");
 	ck_assert_str_eq(get_string(confs[OPT_PORT]), "1234");
@@ -79,31 +91,88 @@ START_TEST(test_client_conf)
 	ck_assert_str_eq(get_string(confs[OPT_SSL_PEER_CN]), "my_cn");
 	ck_assert_str_eq(get_string(confs[OPT_SSL_KEY]), "/ssl/key/path");
 	ck_assert_str_eq(get_string(confs[OPT_CA_CSR_DIR]), "/csr/dir");
-	confs_free(&confs);
-	ck_assert_int_eq(free_count, alloc_count);
+	tear_down(&confs);
 }
 END_TEST
 
-START_TEST(test_client_includes)
+static void assert_incexc(struct strlist **s, const char *path, int flag)
+{
+	if(!path)
+	{
+		ck_assert_int_eq(*s==NULL, 1);
+		return;
+	}
+	ck_assert_str_eq((*s)->path, path);
+	ck_assert_int_eq((*s)->flag, flag);
+	*s=(*s)->next;
+}
+
+static void assert_include(struct strlist **s, const char *path)
+{
+	assert_incexc(s, path, 1);
+}
+
+static void assert_exclude(struct strlist **s, const char *path)
+{
+	assert_incexc(s, path, 0);
+}
+
+START_TEST(test_client_includes_excludes)
 {
 	const char *buf=MIN_CLIENT_CONF
+		"exclude=/z\n"
+		"exclude=/a/b\n"
 		"include=/a/b/c\n"
 		"include=/x/y/z\n"
 		"include=/r/s/t\n"
+		"include=/a\n"
 	;
-	struct conf **confs;
 	struct strlist *s;
-	alloc_counters_reset();
-	confs=confs_alloc();
-	confs_init(confs);
+	struct conf **confs;
+	confs=setup();
 	ck_assert_int_eq(conf_load_global_only_buf(buf, confs), 0);
 	s=get_strlist(confs[OPT_INCLUDE]);
-	ck_assert_str_eq(s->path, "/a/b/c"); s=s->next;
-	ck_assert_str_eq(s->path, "/r/s/t"); s=s->next;
-	ck_assert_str_eq(s->path, "/x/y/z"); s=s->next;
-	ck_assert_int_eq(s==NULL, 1);
-	confs_free(&confs);
-	ck_assert_int_eq(free_count, alloc_count);
+	assert_include(&s, "/a");
+	assert_include(&s, "/a/b/c");
+	assert_include(&s, "/r/s/t");
+	assert_include(&s, "/x/y/z");
+	assert_include(&s, NULL);
+	s=get_strlist(confs[OPT_EXCLUDE]);
+	assert_exclude(&s, "/a/b");
+	assert_exclude(&s, "/z");
+	assert_exclude(&s, NULL);
+	s=get_strlist(confs[OPT_STARTDIR]);
+	assert_include(&s, "/a");
+	assert_include(&s, "/r/s/t");
+	assert_include(&s, "/x/y/z");
+	assert_include(&s, NULL);
+	s=get_strlist(confs[OPT_INCEXCDIR]);
+	assert_include(&s, "/a");
+	assert_exclude(&s, "/a/b");
+	assert_include(&s, "/a/b/c");
+	assert_include(&s, "/r/s/t");
+	assert_include(&s, "/x/y/z");
+	assert_exclude(&s, "/z");
+	assert_include(&s, NULL);
+	tear_down(&confs);
+}
+END_TEST
+
+static const char *include_failures[] = {
+	MIN_CLIENT_CONF "include=not_absolute\n",
+	MIN_CLIENT_CONF "include=/\ninclude=/\n"
+};
+
+START_TEST(test_client_include_failures)
+{
+	struct conf **confs;
+	FOREACH(include_failures)
+	{
+		confs=setup();
+		ck_assert_int_eq(conf_load_global_only_buf(include_failures[i],
+			confs), -1);
+		tear_down(&confs);
+	}
 }
 END_TEST
 
@@ -118,7 +187,8 @@ Suite *suite_conffile(void)
 
 	tcase_add_test(tc_core, test_conf_get_pair);
 	tcase_add_test(tc_core, test_client_conf);
-	tcase_add_test(tc_core, test_client_includes);
+	tcase_add_test(tc_core, test_client_includes_excludes);
+	tcase_add_test(tc_core, test_client_include_failures);
 	suite_add_tcase(s, tc_core);
 
 	return s;

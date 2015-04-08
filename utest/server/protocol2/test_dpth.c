b38e51d549e7755c5c734e9259502adf899eaf98
graham <graham@space.grke.net>
2015-04-08 12:17:20 +0000

    Some effort on dpth test.

--- utest/server/protocol2/test_dpth.c
+++ utest/server/protocol2/test_dpth.c
@@ -5,8 +5,11 @@
 #include "../../test.h"
 #include "../../../src/alloc.h"
 #include "../../../src/fsops.h"
+#include "../../../src/hexmap.h"
+#include "../../../src/iobuf.h"
 #include "../../../src/lock.h"
 #include "../../../src/server/protocol2/dpth.h"
+#include "../../../src/protocol2/blk.h"
 
 static const char *lockpath="utest_dpth";
 
@@ -22,6 +25,7 @@ static void assert_components(struct dpth *dpth,
 static struct dpth *setup(void)
 {
 	struct dpth *dpth;
+	hexmap_init();
 	fail_unless(recursive_delete(lockpath, "", 1)==0);
 	fail_unless((dpth=dpth_alloc(lockpath))!=NULL);
 	assert_components(dpth, 0, 0, 0, 0);
@@ -35,19 +39,75 @@ static void tear_down(struct dpth **dpth)
 	fail_unless(free_count==alloc_count);
 }
 
+static int write_to_dpth(struct dpth *dpth, const char *savepathstr)
+{
+	int ret;
+	struct iobuf wbuf;
+	struct blk *blk=blk_alloc();
+	savepathstr_to_bytes(savepathstr, blk->savepath);
+	wbuf.buf=strdup_w("abc", __FUNCTION__);
+	wbuf.len=3;
+	ret=dpth_fwrite(dpth, &wbuf, blk);
+	free_w(&wbuf.buf);
+	blk_free(&blk);
+	return ret;
+}
+
+static void do_fork(void)
+{
+	switch(fork())
+	{
+		case -1: fail_unless(0==1);
+			 break;
+		case 0: // Child.
+		{
+			int x;
+			struct dpth *dpth;
+			dpth=dpth_alloc(lockpath);
+			dpth->need_data_lock=1;
+			dpth_init(dpth);
+			dpth_mk(dpth);
+			x=write_to_dpth(dpth, "0000/0000/0001");
+printf("CHILD %d %d\n", x, dpth->tert);
+			sleep(2);
+			dpth_free(&dpth);
+			exit(0);
+		}
+		default: break;
+	}
+	// Parent.
+}
+
 START_TEST(test_simple)
 {
 	struct dpth *dpth;
 	dpth=setup();
 	dpth->need_data_lock=1;
+	fail_unless(dpth_init(dpth)==0);
 	fail_unless(dpth_mk(dpth)!=NULL);
 	fail_unless(dpth->head!=NULL);
 	fail_unless(dpth->head->lock!=NULL);
 	fail_unless(dpth->head->lock->status==GET_LOCK_GOT);
-/* FIX THIS
+	fail_unless(write_to_dpth(dpth, "0000/0000/0000")==0);
+
+	do_fork();
+	sleep(1);
+/*
+	dpth_init(dpth);
+	dpth_mk(dpth);
+	fail_unless(dpth->head->lock->status==GET_LOCK_GOT);
+printf("p: %p\n", dpth->head);
+printf("p: %p\n", dpth->tail);
+	assert_components(dpth, 0, 0, 2, 0);
+	dpth_free(&dpth);
+*/
+fail_unless(1==0);
+/*
 	dpth->need_data_lock=1;
+	dpth->tert++;
 	fail_unless(dpth_mk(dpth)!=NULL);
-	fail_unless(dpth->tail->lock->status==GET_LOCK_NOT_GOT);
+	fail_unless(dpth->tail->lock->status==GET_LOCK_GOT);
+	assert_components(dpth, 0, 0, 2, 0);
 */
 	tear_down(&dpth);
 }

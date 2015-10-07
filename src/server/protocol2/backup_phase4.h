8ffdc39eed3fe5c5a80afaa21a9216d0b0cbf675
Graham Keeling <grke@grke.net>
2015-10-07 12:04:28 +0000

    Merge dedup_group client dindexes together in preparation for deletion of old data files.
    Change-Id: Iabf76fa2d51a75a8a402b27a8388b6e3836621b1

--- src/server/protocol2/backup_phase4.h
+++ src/server/protocol2/backup_phase4.h
@@ -4,14 +4,14 @@
 extern int backup_phase4_server_protocol2(struct sdirs *sdirs,
 	struct conf **confs);
 extern int regenerate_client_dindex(struct sdirs *sdirs);
+extern int merge_dindexes(const char *dst, const char *srca, const char *srcb);
+extern int merge_files_in_dir(const char *final, const char *fmanifest,
+	const char *srcdir, uint64_t fcount,
+	int merge(const char *dst, const char *src, const char *srcb));
 
 #ifdef UTEST
 extern int merge_sparse_indexes(const char *dst,
 	const char *srca, const char *srcb);
-extern int merge_files_in_dir(const char *final, const char *fmanifest,
-	const char *srcdir, uint64_t fcount,
-	int merge(const char *dst, const char *src, const char *srcb));
-extern int merge_dindexes(const char *dst, const char *srca, const char *srcb);
 extern int gzprintf_dindex(struct fzp *fzp, uint64_t *dindex);
 #endif
 
